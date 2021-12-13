621793e61c79f086c033c292f03e61c56016e2c7
Vladimir <malleerx@gmail.com>
2021-12-13 12:31:01 +0300

    Init commit

---
+++ src/test/java/com/malerx/mctester/MctesterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malerx.mctester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MctesterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
