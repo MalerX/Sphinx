f1fde57e07ac9900376ebb4376de2ca67f3bf503
Vladimir <malleerx@gmail.com>
2021-12-21 12:31:44 +0300

    Deep comparison implemented JSON's.

--- src/main/java/com/malerx/mctester/service/validator/ButcherImpl.java
+++ src/main/java/com/malerx/mctester/service/validator/ButcherImpl.java
@@ -4,15 +4,11 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.JsonNodeFactory;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -26,10 +22,12 @@ public class ButcherImpl implements Butcher {
     public @NonNull String butchAndCompare(@NonNull String expected, @NonNull String received) throws JsonProcessingException {
         JsonNode expectedNode = mapper.readTree(expected);
         JsonNode receivedNode = mapper.readTree(received);
-        if (expectedNode.equals(receivedNode)) {
+        if (expectedNode.size() != receivedNode.size()) {
+            return String.format("The expected message format does not match the received one: %s -- %s", expected, received);
+        } else if (!expectedNode.equals(receivedNode)) {
             return deepCompare(expectedNode, receivedNode);
         }
-        return "expected and received answers equals.";
+        return "Successful. Expected and received answers equals.";
     }
 
     @NonNull
@@ -39,9 +37,11 @@ public class ButcherImpl implements Butcher {
             Iterator<Map.Entry<String, JsonNode>> iterator = expectedNode.fields();
             while (iterator.hasNext()) {
                 Map.Entry<String, JsonNode> entry = iterator.next();
-                builder.append(
-                        deepCompare(entry.getValue(), receivedNode.findValue(entry.getKey()))
-                ).append("\t");
+                builder
+                        .append(String.format("%s: ", entry.getKey()))
+                        .append(
+                                deepCompare(entry.getValue(), receivedNode.findValue(entry.getKey()))
+                        ).append("\t");
             }
         } else if (expectedNode.isArray()) {
             ArrayNode arrayNodeExpected = ((ArrayNode) expectedNode);
@@ -50,9 +50,10 @@ public class ButcherImpl implements Butcher {
             for (int i = 0; i < arraySize; i++) {
                 JsonNode nodeExp = arrayNodeExpected.get(i);
                 JsonNode nodeRec = arrayNodeReceived.get(i);
-                builder.append(
-                        compare(nodeExp, nodeRec)
-                ).append("\t");
+                builder
+                        .append(String.format("array[%d]: ", i))
+                        .append(deepCompare(nodeExp, nodeRec))
+                        .append("\t");
             }
         } else if (expectedNode.isValueNode()) {
             builder.append(
@@ -64,19 +65,14 @@ public class ButcherImpl implements Butcher {
 
     private String compare(JsonNode expected, JsonNode received) {
         StringBuilder builder = new StringBuilder();
-        Iterator<String> fieldsIterator = expected.fieldNames();
-        while (fieldsIterator.hasNext()) {
-            String fieldName = fieldsIterator.next();
-            String one = expected.get(fieldName).asText();
-            String two = received.get(fieldName).asText();
+        String one = expected.textValue();
+        String two = received.textValue();
 
-            if (one.equals(two)) {
-                builder.append(String.format("fields %s equals", fieldName)).append("\t");
-            } else
-                builder.append(String.format("NON-EQUALS: %s=%s -- %s=%s", fieldName, one, fieldName, two))
-                        .append("\t");
-        }
-        return builder.toString().trim();
+        if (one.equals(two)) {
+            builder.append("equals");
+        } else
+            builder.append(String.format("NON EQUALS: expected = %s -- received = %s", one, two));
+        return builder.toString();
     }
 
     public static void main(String[] args) throws JsonProcessingException {
@@ -85,7 +81,7 @@ public class ButcherImpl implements Butcher {
                 {"books":{"book":[{"title":"CPP","author":"Milton","year":"2008","price":"456.00"},{"title":"JAVA","author":"Gilson","year":"2002","price":"456.00"}]}}
                 """;
         String received = """
-                {"books":{"book":[{"title":"CPP","author":"Milton","year":"2008","price":"456.00"},{"title":"JAVA","author":"Gilson","year":"2002","price":"456.00"}]}}
+                {"books":{"book":[{"title":"CPP","author":"Jhon Boy","year":"2021","price":"456.00"},{"title":"Phyton","author":"Gilson","year":"2002","price":"456.00"}]}}
                 """;
 
         String s = butcher.butchAndCompare(expected, received);
---
+++ src/test/java/com/malerx/mctester/service/validator/ButcherImplTest.java
@@ -0,0 +1,13 @@
+package com.malerx.mctester.service.validator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ButcherImplTest {
+
+
+    @Test
+    void butchAndCompare() {
+    }
+}
